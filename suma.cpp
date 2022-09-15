d55f2f7a2137b750de3304400dc1108f9d172372
jorky <jorkyproano20@gmail.com>
2022-09-15 18:26:01 -0500

    version3

--- suma.cpp
+++ suma.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 using namespace std;
 int main ()
-{
-	float A,B,C;
-        cout<< "ingrese dos numeros A B:";
-	cin>>A>>B;
-	C=A+B;
+{	float x,y,z;
+        cout<< "ingrese dos numerosseparados de espacio x y:";
+	cin>>x>>y;
+	z=x+y;
 	cout<<"El resultado es: ";
-	cout<<C;
+	cout<<z<<endl;
 	return 0;
 }
