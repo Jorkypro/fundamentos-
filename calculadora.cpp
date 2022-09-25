b260415d487e7d40872907bcbf62c7165017c142
Jorkypro <113040935+Jorkypro@users.noreply.github.com>
2022-09-24 21:52:27 -0500

    Update calculadora.cpp

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,24 +1,15 @@
-# incluir < iostream >
+#include <iostream>
 utilizando el espacio de  nombres  estándar ;
 
 int  principal ()
 {
-	flotante x,y,s,m,d,r;
-	// Ingreso de datos
-	cout<< " Ingreso en valor de x=: " ;
-	cin>>x;
-	cout<< " Ingreso en valor de y=: " ;
-	// Operaciones
-	cin>>y;
-	s=x+y;
-	m=x*y;
-	d=x/y;
-	r=xy;
-	// Muestra los resultados.
-	cout<< " Las sumas de " <<x<< " + " <<y<< " = " <<s<<endl;
-	cout<< " Las multiplicaciones de " <<x<< " * " <<y<< " = " <<m<<endl;
-	cout<< " Las divisiones de " <<x<< " / " <<y<< " = " <<d<<endl;
-	cout<< " Las resta de " <<x<< " - " <<y<< " = " <<r<<endl;
-	devolver  0 ;
 
+ flotante x, y, z;
+cout<< " Ingreso 2 numero xy: " ;
+cin>>x>>y;
+z=x+y;
+cout<< " El resultado es: " ;
+cout<<z<<endl;
+
+ devolver  0 ;
 }
